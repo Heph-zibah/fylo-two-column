6c690ecb1b4e4be32db147746f035efdbb3e2584
oaDaramola <oluwatosinhephzibah@gmail.com>
2024-07-05 13:14:53 +0100

    update file

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <h1>npx tailwindcss init -p</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      color: {
+        veryDarkBlue: "hsl(243, 87%, 12%)",
+        desaturatedBlue: "hsl(238, 22%, 44%)",
+        brightBlue: "hsl(224, 93%, 58%)",
+        moderateCyan: "hsl(170, 45%, 43%)",
+        lightGrayishBlue: "hsl(240, 75%, 98%)",
+        lightGray: " hsl(0, 0%, 75%)",
+      },
+    },
+    fontFamily: {
+      sans: ["Open Sans", "sans-serif"],
+      raleway: ["Raleway", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
